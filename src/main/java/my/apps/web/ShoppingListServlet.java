ef575cc02bdf1b8f89e48c25a76c681e630989b1
Dunch <andrei.danci@gmail.com>
2017-03-02 21:37:18 +0200

    shoping list site add application

--- src/main/java/my/apps/web/ShoppingListServlet.java
+++ src/main/java/my/apps/web/ShoppingListServlet.java
@@ -14,6 +14,18 @@ public class ShoppingListServlet extends HttpServlet {
     private int counter;
     private static final String LIST_ACTION = "list";
 
+    Item[] items={
+
+            new Item("Paine", "150"),
+            new Item("Mere", "3"),
+            new Item("Mere", "15"),
+            new Item("Pasta De Dinti", "2"),
+            new Item("Pasta De Icre", "2"),
+
+    };
+
+    int contor = 5;
+
     @Override
     public void service(HttpServletRequest request, HttpServletResponse response) {
         System.out.println("my shopping list service called now.");
@@ -23,15 +35,43 @@ public class ShoppingListServlet extends HttpServlet {
 
         if (action != null && action.equals(LIST_ACTION)) {
             listAction(request, response);
+        } else if (action != null && action.equals("add")){
+
+            addAction(request, response);
         }
 
         System.out.println("I was used " + counter + " times!");
     }
 
+    private void addAction(HttpServletRequest request, HttpServletResponse response) {
+
+        String produs = request.getParameter("produs");
+        String cantitate = request.getParameter("cantitate");
+
+        Item itemuNou = new
+
+
+    }
+
     private void listAction(HttpServletRequest request, HttpServletResponse response) {
+
+
+        String jsonResponse = "[";
+        for (int i=0; i< items.length; i++){
+            String nume = items[i].getNume();
+            String cantitate = items[i].getCantitate();
+            String element = "{\"nume\":\"" + nume + "\", \"cantitate\": " + cantitate + "}";
+            if (i < items.length - 1)
+            jsonResponse += ",";
+        };
+            jsonResponse += "]";
+            returnJsonResponse(response, jsonResponse);
         String jsonResponse = "[\n" +
-                "  {\"nume\": \"Nimic\", \"cantitate\": -1},\n" +
-                "]";
+                "  {\"nume\": \"Paine\", \"cantitate\": 70},\n" +
+               "  {\"nume\": \"Suc\", \"cantitate\": 3},\n" +
+                "  {\"nume\": \"Mere\", \"cantitate\": 10},\n" +
+                "  {\"nume\": \"Pasta de dinti\", \"cantitate\": 2}\n" +
+                "] ";
         returnJsonResponse(response, jsonResponse);
     }
 
